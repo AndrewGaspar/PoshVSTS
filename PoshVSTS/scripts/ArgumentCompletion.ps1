95353d0eaa2f91a93e01cbac2e775c2251759a05
Andrew Gaspar <andrew.gaspar@outlook.com>
2016-01-10 14:55:48 -0800

    Completion for Team Name

--- PoshVSTS/scripts/ArgumentCompletion.ps1
+++ PoshVSTS/scripts/ArgumentCompletion.ps1
@@ -25,6 +25,34 @@ function CompleteProjectId {
     }
 }
 
+function GetFakeProject($fakeBoundParameters) {
+    if($fakeBoundParameters.ProjectId) {
+        return $fakeBoundParameters.ProjectId
+    } elseif($fakeBoundParameters.ProjectName) {
+        return $fakeBoundParameters.ProjectName
+    }
+}
+
+function CompleteTeamName {
+    param($commandName,
+        $parameterName,
+        $wordToComplete,
+        $commandAst,
+        $fakeBoundParameters)
+        
+    if($fakeBoundParameters.Instance) {
+        & {
+            if($fakeBoundParameters.ProjectName) {
+                Get-VstsProjectTeam $fakeBoundParameters.Instance $fakeBoundParameters.ProjectName -ChunkSize 1000
+            } elseif($fakeBoundParameters.ProjectId) {
+                Get-VstsProjectTeam $fakeBoundParameters.Instance $fakeBoundParameters.ProjectId -ChunkSize 1000
+            }
+        } | % { $_.name } |
+            ? { $_ -like "$wordToComplete*" } |
+            % { if($_.Contains(" ")) { "`"$_`"" } else { $_ } }
+    }
+}
+
 Register-ArgumentCompleter `
     -CommandName @(Get-Command "*-VstsProject") `
     -ParameterName Id `
@@ -34,4 +62,9 @@ Register-ArgumentCompleter `
     -CommandName @(Get-Command "*-Vsts*") `
     -ParameterName ProjectId `
     -ScriptBlock $function:CompleteProjectId
+    
+Register-ArgumentCompleter `
+    -CommandName @(Get-Command "*-VstsProjectTeam") `
+    -ParameterName Name `
+    -ScriptBlock $function:CompleteTeamName
     
